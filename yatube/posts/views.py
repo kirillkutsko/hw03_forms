01800590855e87347b574295b1b0107e58d0a6f4
Кирилл Куцко <eads@mail.ru>
2022-04-22 22:12:22 +0300

    Сдача 2

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,13 +1,13 @@
-from datetime import datetime
-
+from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.db.models import SlugField
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
-
 from .forms import PostForm
-from .models import Group, Post, User
+from .models import Group, Post
+
+User = get_user_model()
 
 
 def index(request: HttpRequest) -> HttpResponse:
@@ -22,7 +22,7 @@ def index(request: HttpRequest) -> HttpResponse:
 def group_posts(request: HttpRequest, slug: SlugField) -> HttpResponse:
     """Вернуть HttpResponse объекта страницы группы"""
     group = get_object_or_404(Group, slug=slug)
-    post_list = group.posts.all()
+    post_list = group.posts.select_related("group").all()
     paginator = Paginator(post_list, 10)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
@@ -30,7 +30,7 @@ def group_posts(request: HttpRequest, slug: SlugField) -> HttpResponse:
     return render(request, "posts/group_list.html", context)
 
 
-def profile(request, username=None):
+def profile(request, username):
     author = get_object_or_404(User, username=username)
     post_list = author.posts.all()
     paginator = Paginator(post_list, 10)
@@ -39,64 +39,47 @@ def profile(request, username=None):
     context = {
         "author": author,
         "page_obj": page_obj,
-        "paginator": paginator
     }
     return render(request, 'posts/profile.html', context)
 
 
 def post_detail(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
-    username = post.author
-    context = {"post": post, "username": username}
-    return render(request, 'posts/post_detail.html', context)
+    return render(request, 'posts/post_detail.html', {"post": post})
 
 
 @login_required
 def post_create(request):
-    if request.method == "POST":
-        form = PostForm(request.POST)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.published_date = datetime.now()
-            post.save()
-            return redirect('posts:profile', username=post.author.username)
-    else:
-        form = PostForm()
+    form = PostForm(request.POST or None)
+    if form.is_valid():
+        post = form.save(commit=False)
+        post.author = request.user
+        post.save()
+        return redirect('posts:profile', username=request.user)
     return render(request, 'posts/create_post.html', {'form': form})
 
 
 @login_required
 def post_edit(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
-    username = post.author
-    form = PostForm(instance=post)
-    text = post.text
-    group = post.group
+    form = PostForm(request.POST or None, instance=post)
     is_edit = True
     context = {
         "form": form,
-        "username": username,
-        "text": text,
-        "group": group,
         "post_id": post_id,
         "is_edit": is_edit,
     }
     if post.author != request.user:
-        return redirect("posts:post_detail", post.pk)
-    elif request.method == "POST":
-        form = PostForm(request.POST)
+        return redirect("posts:post_detail", post_id)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
         if form.is_valid:
             res_form = form.save(commit=False)
             res_form.author = request.user
-            if form.cleaned_data["group"]:
-                group = form.cleaned_data["group"]
-            text = form.cleaned_data["text"]
             res_form.save()
             return redirect("posts:post_detail", post.pk)
-    else:
-        return render(
-            request,
-            "posts/create_post.html",
-            context
-        )
+    return render(
+        request,
+        "posts/create_post.html",
+        context
+    )
