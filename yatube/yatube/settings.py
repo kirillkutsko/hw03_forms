b79d6a65398d05013b7ac9920c1f9cd807c9f8ad
Кирилл Куцко <eads@mail.ru>
2022-05-01 18:26:48 +0300

    _

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -12,7 +12,12 @@ EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 
 EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    '[::1]',
+    'testserver',
+]
 
 LOGIN_URL = 'users:login'
 
